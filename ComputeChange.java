834679b68725996979cd6e75d13930b02825b05d
christ <251495054@qq.com>
2023-04-11 16:40:45 +0000

    qwe

---
+++ ComputeChange.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ComputeChange {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter an amount in double,for example 11.56:");
+        double amount=input.nextDouble();
+        int remainingAmount=(int)(amount*100);
+        int numberOfOneDollars=remainingAmount/100;
+        remainingAmount=remainingAmount%100;
+        int numberOfQuarters=remainingAmount/25;
+        remainingAmount=remainingAmount%25;
+        
+        
+
+    }
+    
+}
