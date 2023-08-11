e21183543c74edfa4e4ddaa2868c0ff2f5147de7
abnormalaimer <251495054@qq.com>
2023-08-11 14:27:16 +0800

    2023811

---
+++ collections/Stack/Stack.java
@@ -0,0 +1,14 @@
+package collections.Stack;
+import java.util.*;
+
+public class Stack<T> {
+    private Deque<T> storage=new ArrayDeque<>();
+    public void push(T v){storage.push(v);}
+    public T peek(){return storage.peek();}
+    public T pop(){return storage.pop();}
+    public boolean isEmpty(){return storage.isEmpty();}
+    @Override
+    public String toString(){
+        return storage.toString();
+    }    
+}
