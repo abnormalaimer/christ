b7486bdcfa511a3423fd84027a11746c9cff75b9
abnormalaimer <251495054@qq.com>
2023-08-13 11:30:57 +0800

    2023813

---
+++ functional/MethodReferences.java
@@ -0,0 +1,37 @@
+package functional;
+import java.util.*;
+interface Callable{
+    void call(String s);
+}
+
+class Describe {
+    void show(String msg){
+        System.out.println(msg);
+    }
+}
+public class MethodReferences {
+    static void hello(String name){
+        System.out.println("hello,"+name);
+    }
+    static class Description{
+        String about;
+        Description(String desc){about=desc;}
+        void help(String msg){
+            System.out.println(about+" "+msg);
+        }
+    }
+    static class Helper{
+        static void assist(String msg){
+            System.out.println(msg);
+        }
+    }
+
+public static void main(String[] args) {
+    Describe d=new Describe();
+    Callable c=d::show;
+    c.call("call()");
+    c=MethodReferences::hello;
+    c.call("bob");
+    c=Helper::assist;
+    c.call("help!");
+}}
