1e3f335d72517e8b91cc0e1bf112a3fbe00f1726
abnormalaimer <251495054@qq.com>
2023-08-23 00:34:25 +0800

    2023.12.17

---
+++ streams/Bubbles.java
@@ -0,0 +1,9 @@
+package streams;
+
+import java.util.stream.Stream;
+
+public class Bubbles {
+    public static void main(String[] args) {
+        Stream.generate(Bubble::bubble).limit(5).forEach(System.out::println);
+    }
+}
